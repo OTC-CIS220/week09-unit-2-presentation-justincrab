7d4ce6dd92fb02082575744cefd92f3ca159bb7e
Justin Crabtree <justincrab@gmail.com>
2018-03-22 13:58:47 -0500

    issa game

---
+++ Roll-a-ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        // find the difference between camera position and ball's position
+        offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {//lateupdate is called after everything else has been run
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public int score =0;
+    public float speed;
+    public Text scoreText;
+    public Text winText;
+    private Rigidbody rb; //variable that holds reference to 'Rigidbody' compononent in unity
+    public GameObject[] pickups = GameObject.FindGameObjectsWithTag("Pick Up");
+    private void Start()
+    {
+        winText.text = "";
+        // first frame, link rb to rigidbody component in unity
+        rb = GetComponent<Rigidbody>();
+        SetScoreText();
+    }
+    private void FixedUpdate()
+    {
+        //every frame, input is read in, put into vector3, which is passed to add force
+        // gets movement input from user/keyboard
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //we collide with 'other', if 'other's tag matches, deactivate it.
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score += 1;
+            SetScoreText();
+        }
+    }
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 31 )
+        {
+            winText.text = "You win!";
+            //foreach (var obj in pickups)
+            //    obj.SetActive(true);            
+            score = 0;
+            speed = speed * 2;
+            SetScoreText();
+            winText.text = "";
+        }
+    }
+}
